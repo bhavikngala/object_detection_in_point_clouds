corrected pre-normalization and activation

networks/networks.py
@@ -6,30 +6,41 @@ from networks.blocks import Bottleneck_3, Bottleneck_6, Upsample
 
 class PointCloudDetector(nn.Module):
 
+	# res_block_layers = list if number of channels in the first conv layer of each res_block
+	# up_sample_layers = list of tuple of number of channels input deconv and conv layers
 	def __init__(self, res_block_layers, up_sample_layers):
-		# res_block_layers = list if number of channels in the first conv layer of each res_block
-		# up_sample_layers = list of tuple of number of channels input deconv and conv layers
+
+		super(PointCloudDetector, self).__init__()
 
 		# keep padding in mind since at all convolutions are "SAME"
 		# except for layers where downsampling happens
-		self.conv1 = nn.Conv2d(in_channels=36, out_channels=32, kernel_size=3, padding=1, bias=False)
+
+		self.conv1 = nn.Conv2d(in_channels=38, out_channels=32, kernel_size=3, padding=1, bias=False)
 		self.bn1 = nn.BatchNorm2d(32)
 
 		self.conv2 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1, bias=False)
+		self.bn2 = nn.BatchNorm2d(32)
 
 		self.res_block1 = Bottleneck_3(in_channels = 32, out_channels=res_block_layers[0])
-		self.res_block2 = Bottleneck_6(in_channels = 4 * res_block_layers[0], out_channels=res_block_layers[1])
-		self.res_block3 = Bottleneck_6(in_channels = 4 * res_block_layers[1], out_channels=res_block_layers[2])
-		self.res_block4 = Bottleneck_3(in_channels = 4 * res_block_layers[2], out_channels=res_block_layers[3])
+		self.bn_res_block1 = nn.BatchNorm2d(4 * res_block_layers[0])
 
-		self.bn3 = nn.BatchNorm2d(res_block_layers[3])
+		self.res_block2 = Bottleneck_6(in_channels = 4 * res_block_layers[0], out_channels=res_block_layers[1])
+		self.bn_res_block2 = nn.BatchNorm2d(4 * res_block_layers[1])
+
+		self.res_block3 = Bottleneck_6(in_channels = 4 * res_block_layers[1], out_channels=res_block_layers[2])
+		self.bn_res_block3 = nn.BatchNorm2d(4 * res_block_layers[2])
+
+		self.res_block4 = Bottleneck_3(in_channels = 4 * res_block_layers[2], out_channels=res_block_layers[3])
+		self.bn_res_block4 = nn.BatchNorm2d(4 * res_block_layers[3])
 		
-		self.conv4 = nn.Conv2d(in_channels=res_block_layers[3], out_channels=196, kernel_size=1, bias=False)
+		self.conv4 = nn.Conv2d(in_channels=4*res_block_layers[3], out_channels=196, kernel_size=1, bias=False)
+		self.bn4 = nn.BatchNorm2d(196)
 
 		self.upsample1 = Upsample(in_channels = up_sample_layers[0], out_channels = 128)
-		self.upsample2 = Upsample(in_channels = up_sample_layers[1], out_channels = 96)
+		self.bn_upsample1 = nn.BatchNorm2d(128)
 
-		self.bn4 = nn.BatchNorm2d(96),
+		self.upsample2 = Upsample(in_channels = up_sample_layers[1], out_channels = 96)
+		self.bn_upsample2 = nn.BatchNorm2d(96)
 		
 		self.conv5 = nn.Conv2d(in_channels=96, out_channels=96, kernel_size=3, padding=1, bias=False)
 		self.bn5 = nn.BatchNorm2d(96)
@@ -59,21 +70,35 @@ class PointCloudDetector(nn.Module):
 		x = self.relu(x)
 
 		x = self.conv2(x)
+		x = self.bn2(x)
+		x = self.relu(x)
 
 		x = self.res_block1(x)
-		res_2 = self.res_block2(x)
-		res_3 = self.res_block3(res_2)
-		x = self.res_block4(res_3)
+		x = self.bn_res_block1(x)
+		x = self.relu(x)
 
-		x = self.bn3(x)
+		res_2 = self.res_block2(x)
+		res_2 = self.bn_res_block2(res_2)
+		res_2 = self.relu(res_2)
+
+		res_3 = self.res_block3(res_2)
+		res_3 = self.bn_res_block2(res_3)
+		res_3 = self.relu(res_3)
+
+		x = self.res_block4(res_3)
+		x = self.bn_res_block4(x)
 		x = self.relu(x)
 
 		x = self.conv4(x)
+		x = self.bn4(x)
+		x = relu(x)
 
 		x = self.upsample1(x, res_3)
-		x = self.upsample2(x, res_2)
+		x = self.bn_upsample1(x)
+		x = self.relu(x)
 
-		x = self.bn4(x)
+		x = self.upsample2(x, res_2)
+		x = self.bn_upsample2(x)
 		x = self.relu(x)
 
 		x = self.conv5(x)
@@ -97,6 +122,5 @@ class PointCloudDetector(nn.Module):
 
 		loc = self.conv_loc(x)
 		loc = self.loc_act(loc)
-
-		# may want to reshape the tensors before returning
+		
 		return cla, loc