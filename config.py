bb492d61cdcbcf277ab22042c9eeca76b61365be
Bhavik N Gala <bhavikna@buffalo.edu>
2018-10-14 19:26:21 -0400

    added lidar to camera cordinate system and vice versa conversion matrices

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import torch
 
 rootDir = './../data/KITTI_BEV'
-batchSize = 2
+batchSize = 3
 
 gridConfig = {
 	'x':(0, 70),
@@ -19,7 +19,7 @@ res_block_layers = [24, 48, 64, 96]
 up_sample_layers = [(196, 256), (128, 192)]
 
 # training parameters
-learningRate = 1e-4
+learningRate = 1e-2
 epochs = 200
 
 # gamma for focal loss
@@ -45,3 +45,23 @@ testlog = './loss/test.txt'
 
 # string for log
 logString = 'epoch: {:05d} | cla loss: {:.4f} | loc loss: {:.4f} | total loss: {:.4f} \n'
+
+# camera and lidar conversion matrices
+MATRIX_P2 = ([
+	[719.787081,    0.,            608.463003, 44.9538775],
+	[0.,            719.787081,    174.545111, 0.1066855],
+	[0.,            0.,            1.,         3.0106472e-03],
+	[0.,            0.,            0.,         0]
+])
+MATRIX_T_VELO_2_CAM = ([
+	[7.49916597e-03, -9.99971248e-01, -8.65110297e-04, -6.71807577e-03],
+	[1.18652889e-02, 9.54520517e-04, -9.99910318e-01, -7.33152811e-02],
+	[9.99882833e-01, 7.49141178e-03, 1.18719929e-02, -2.78557062e-01],
+	[0, 0, 0, 1]
+ ])
+MATRIX_R_RECT_0 = ([
+	[0.99992475, 0.00975976, -0.00734152, 0],
+	[-0.0097913, 0.99994262, -0.00430371, 0],
+	[0.00729911, 0.0043753, 0.99996319, 0],
+	[0, 0, 0, 1]
+])
