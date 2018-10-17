2317517da7bf71e44ad5a2eaff43e8f4b2d64922
Bhavik N Gala <bhavikna@buffalo.edu>
2018-10-17 13:28:23 -0400

    pin memory, num workers

--- datautils/dataloader.py
+++ datautils/dataloader.py
@@ -21,17 +21,17 @@ def lidarDatasetLoader(rootDir, batchSize, gridConfig, objtype):
 	'''
 	trainLoader = DataLoader(
 		LidarLoader(join(rootDir, 'train'), gridConfig, objtype),
-		batch_size = batchSize, shuffle=True,
+		batch_size = batchSize, shuffle=True, num_workers=2,
 		collate_fn=collate_fn, pin_memory=True
 	)
 	validationLoader = DataLoader(
 		LidarLoader(join(rootDir, 'val'), gridConfig, objtype),
-		batch_size = batchSize, shuffle=True,
+		batch_size = batchSize, shuffle=True, num_workers=2,
 		collate_fn=collate_fn, pin_memory=True
 	)
 	testLoader = DataLoader(
 		LidarLoader(join(rootDir, 'test'), gridConfig, train=False),
-		batch_size = batchSize, shuffle=True,
+		batch_size = batchSize, shuffle=True, num_workers=2,
 		collate_fn=collate_fn, pin_memory=True
 	)
 	return trainLoader, validationLoader, testLoader
