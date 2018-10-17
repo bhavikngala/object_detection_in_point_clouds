bb2a8d74dc96c6e53867dad085c22ddd025165cb
Bhavik N Gala <bhavikna@buffalo.edu>
2018-10-17 13:27:51 -0400

    removed old code, convert camera to lidar system

--- datautils/kittiUtils.py
+++ datautils/kittiUtils.py
@@ -1,79 +1,53 @@
-import torch
 import numpy as np
 import config as cnf
 
-# source: https://github.com/jeasinema/VoxelNet-tensorflow/blob/master/utils/utils.py
+'''
+References:
+1. http://www.mrt.kit.edu/z/publ/download/2013/GeigerAl2013IJRR.pdf
+2. https://github.com/Mi-lo/pykitti
+'''
 
-def angle_in_limit(angle):
-	# To limit the angle in -pi/2 - pi/2
-	limit_degree = 5
-	while angle >= np.pi / 2:
-		angle -= np.pi
-	while angle < -np.pi / 2:
-		angle += np.pi
-	if abs(angle + np.pi / 2) < limit_degree / 180 * np.pi:
-		angle = np.pi / 2
-	return angle
+def readR0AndTRVeloToCamMatrices(filename):
+	'''
+	Reads R0 and TR_velo_to_cam matrices from calibration file
+	params : Filename where the calibration data is store
+	returns: numpy arrays of R0 and Tr_velo_to_cam 
+	'''
+	d = {}
+	with open(filename) as f:
+		for line in f.readlines():
+			key, value = line.split(':', 1)
+			d[key] = np.array([float(v) for v in value])
 
+	R0, Tr_velo_to_cam = np.zeros((4,4)), np.zeros((4,4))
+	R0[3,3], Tr_velo_to_cam[3,3] = 1
+	
+	R0[:3, :3] = d['R0'].reshape(3,3)
+	Tr_velo_to_cam[:3, :] = d['Tr_velo_to_cam'].reshape(3, 4)
 
-def camera_to_lidar(x, y, z):
-	p = np.array([x, y, z, 1])
-	p = np.matmul(np.linalg.inv(np.array(cnf.MATRIX_R_RECT_0)), p)
-	p = np.matmul(np.linalg.inv(np.array(cnf.MATRIX_T_VELO_2_CAM)), p)
-	p = p[0:3]
-	return tuple(p)
-
-
-def lidar_to_camera(x, y, z):
-	p = np.array([x, y, z, 1])
-	p = np.matmul(np.array(cnf.MATRIX_T_VELO_2_CAM), p)
-	p = np.matmul(np.array(cnf.MATRIX_R_RECT_0), p)
-	p = p[0:3]
-	return tuple(p)
+	return R0, Tr_velo_to_cam
 
 
-def camera_to_lidar_point(points):
-	# (N, 3) -> (N, 3)
-	N = points.shape[0]
-	points = np.hstack([points, np.ones((N, 1))]).T  # (N,4) -> (4,N)
+def convertCamera0ToLidar(camera0Points, calibFilename):
+	'''
+	Converts 3D points in lidar cordinate system to 3D points in 
+	Camera 0 cordinate system
+	params : np.array, points in lidar system, (N, 7), (x, y, z, h, w, l, ry)
+	params : string, filename of the calibration data
+	returns: np.array, points in camera0 system, (N, 7), (x', y', z', h, w, l, ry')
+	'''
+	R0, Tr_velo_to_cam = readR0AndTRVeloToCamMatrices(calibFilename)
+	# take inverse of the matrices
+	R0_inv = np.linalg.inv(R0)
+	Tr_velo_to_cam_inv = np.linalg.inv(Tr_velo_to_cam)
 
-	points = np.matmul(np.linalg.inv(np.array(cnf.MATRIX_R_RECT_0)), points)
-	points = np.matmul(np.linalg.inv(
-		np.array(cnf.MATRIX_T_VELO_2_CAM)), points).T  # (4, N) -> (N, 4)
-	points = points[:, 0:3]
-	return points.reshape(-1, 3)
+	camPoints = np.ones((camera0Points.shape[0], 4))
+	camPoints[:, :3] = camera0Points[:,:3]
 
+	lidarPoints = np.matmul(R0_inv, np.matmul(Tr_velo_to_cam_inv, camPoints.T))
 
-def lidar_to_camera_point(points):
-	# (N, 3) -> (N, 3)
-	N = points.shape[0]
-	points = np.hstack([points, np.ones((N, 1))]).T
-
-	points = np.matmul(np.array(cnf.MATRIX_T_VELO_2_CAM), points)
-	points = np.matmul(np.array(cnf.MATRIX_R_RECT_0), points).T
-	points = points[:, 0:3]
-	return points.reshape(-1, 3)
-
-
-def camera_to_lidar_box(boxes):
-	# (N, 7) -> (N, 7) x,y,z,h,w,l,r
-	ret = []
-	for box in boxes:
-		x, y, z, h, w, l, ry = box
-		(x, y, z), h, w, l, rz = camera_to_lidar(x, y, z), \
-			h, w, l, -ry - np.pi / 2
-		rz = angle_in_limit(rz)
-		ret.append([x, y, z, h, w, l, rz])
-	return np.array(ret).reshape(-1, 7)
-
-
-def lidar_to_camera_box(boxes):
-	# (N, 7) -> (N, 7) x,y,z,h,w,l,r
-	ret = []
-	for box in boxes:
-		x, y, z, h, w, l, rz = box
-		(x, y, z), h, w, l, ry = lidar_to_camera(x, y, z), \
-			h, w, l, -rz - np.pi / 2
-		ry = angle_in_limit(ry)
-		ret.append([x, y, z, h, w, l, ry])
-	return np.array(ret).reshape(-1, 7)
+	# convert from homogenous points to normal points
+	lidarPoints[:3,:] = lidarPoints[:3,:]/lidarPoints[3,:]
+	
+	camera0Points[:,:3] = lidarPoints.T[:,:3]
+	return camera0Points
