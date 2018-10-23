6cec5b6b6ac24354f553384b4848a77632d40b49
Bhavik N Gala <bhavikna@buffalo.edu>
2018-10-23 10:31:01 -0400

    config changes

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 rootDir = './../data/KITTI_BEV'
 trainRootDir = './../data/preprocessed/train'
@@ -10,6 +11,27 @@ gridConfig = {
 	'res':0.1
 }
 
+Tr_velo_to_cam = np.array([
+		[7.49916597e-03, -9.99971248e-01, -8.65110297e-04, -6.71807577e-03],
+		[1.18652889e-02, 9.54520517e-04, -9.99910318e-01, -7.33152811e-02],
+		[9.99882833e-01, 7.49141178e-03, 1.18719929e-02, -2.78557062e-01],
+		[0, 0, 0, 1]
+	])
+# cal mean from train set
+R0 = np.array([
+		[0.99992475, 0.00975976, -0.00734152, 0],
+		[-0.0097913, 0.99994262, -0.00430371, 0],
+		[0.00729911, 0.0043753, 0.99996319, 0],
+		[0, 0, 0, 1]
+])
+P2 = np.array([[719.787081,         0., 608.463003,    44.9538775],
+               [        0., 719.787081, 174.545111,     0.1066855],
+               [        0.,         0.,         1., 3.0106472e-03],
+			   [0., 0., 0., 0]])
+R0_inv = np.linalg.inv(R0)
+Tr_velo_to_cam_inv = np.linalg.inv(Tr_velo_to_cam)
+P2_inv = np.linalg.pinv(P2)
+
 objtype = 'car'
 
 # res_block_layers = list if number of channels in the first conv layer of each res_block
@@ -19,7 +41,8 @@ res_block_layers = [24, 48, 64, 96]
 up_sample_layers = [(196, 256), (128, 192)]
 
 # training parameters
-learningRate = 1e-2
+lr = 1e-4   # learning rate without step
+slr = 1e-2  # step learning rate
 epochs = 200
 
 # gamma, alpha, epsilon for focal loss
@@ -58,4 +81,4 @@ logString3 = 'epoch: {:03d} | batch:{:04d} | cla loss: None | loc loss: None| to
 # logString = Template('epoch: $e | cla loss: $cl | loc loss: $ll | total loss: $tl \n')
 
 numBatchInQueue = 4
-batchSize = 3
+batchSize = 4
