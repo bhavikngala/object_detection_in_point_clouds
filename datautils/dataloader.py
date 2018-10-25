6c0b08e1db51b4377da6478c5ce3689b073e36ea
Bhavik N Gala <bhavikna@buffalo.edu>
2018-10-24 21:56:29 -0400

    data aug flag bug fix

--- datautils/dataloader.py
+++ datautils/dataloader.py
@@ -294,11 +294,12 @@ class LidarLoader_2(Dataset):
 	No augmentation is done, direct training on the train data
 	This model might overfit but we get a good point to start at
 	'''
-	def __init__(self, directory, objtype, train=True, aug_data=False):
+	def __init__(self, directory, objtype, train=True, augData=False):
 		# load train dataset or test dataset
 		self.train = train
 		self.directory = directory
 		self.objtype = objtype
+		self.augData = augData
 
 		# read all the filenames in the directory
 		self.filenames = [join(directory, f) for f in listdir(directory) \
@@ -351,7 +352,7 @@ class LidarLoader_2(Dataset):
 
 		# augment data
 		if self.train:
-			lidarData, labels[:,1:] = ku.aug_data(lidarData, labels[:,1:], self.aug_data)
+			lidarData, labels[:,1:] = ku.aug_data(lidarData, labels[:,1:], self.augData)
 
 		bev = lidarToBEV(lidarData, cnf.gridConfig)
 		labels1 = np.zeros((labels.shape[0], 7),dtype=np.float32)
