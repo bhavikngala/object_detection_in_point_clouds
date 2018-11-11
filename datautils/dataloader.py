b2f11c0fa269fbd5a2318da861af25f52fd4055e
Bhavik N Gala <bhavikna@buffalo.edu>
2018-11-11 15:23:22 -0500

    removed log in l and w

--- datautils/dataloader.py
+++ datautils/dataloader.py
@@ -93,7 +93,7 @@ class LidarLoader_2(Dataset):
 		labels1[:,0] = labels[:,0]
 		labels1[:,1], labels1[:,2] = np.cos(labels[:,7]), np.sin(labels[:,7])
 		labels1[:,3], labels1[:,4] = labels[:,1], labels[:,2]#x,y
-		labels1[:,5], labels1[:,6] = np.log(labels[:,6]), np.log(labels[:,5])#l,w
+		labels1[:,5], labels1[:,6] = labels[:,6], labels[:,5]#l,w
 
 		# normalize data
 		
