7c2ed757955bd6a0dfc6a9d86240193ce23f469e
Bhavik N Gala <bhavikna@buffalo.edu>
2018-11-23 16:53:26 -0500

    res block changes

--- networks/blocks.py
+++ networks/blocks.py
@@ -16,7 +16,7 @@ class Bottleneck_3_0(nn.Module):
 		# TODO: switch stride=2 between conv1 and conv2 and check results
 		# self.bn1 = nn.BatchNorm2d(out_channels)
 		self.bn1 = nn.BatchNorm2d(in_channels)
-		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False)
 
 		# self.bn2 = nn.BatchNorm2d(out_channels*self.expansion)
 		self.bn2 = nn.BatchNorm2d(out_channels)
@@ -108,29 +108,63 @@ class Bottleneck_6_0(nn.Module):
 		return out
 
 
-class Upsample_1(nn.Module):
-	'''
-	Upsamples the input sample by a factor of 2.
-	First the input is upsample by bilinear interpolation.
-	Then, convolution is applied to the interpolated image.
-	Reference: https://distill.pub/2016/deconv-checkerboard/
-	'''
-	def __init__(self, in_channels, out_channels, output_size=None):
-		super(Upsample, self).__init__()
-		if output_size:
-			self.upsample = nn.Upsample(size=output_size, mode='bilinear')
-		else:
-			self.upsample = nn.Upsample(scale_factor=2,  mode='bilinear')
+class Bottleneck_6_1(nn.Module):
+	expansion = 4
 
-		self.conv_upsample = nn.Conv2d(in_channels[0], out_channels, kernel_size=3, padding=1, bias=False)
-		self.conv1 = nn.Conv2d(in_channels[1], out_channels, kernel_size=1, bias=False)
+	def __init__(self, in_channels, out_channels):
+		super(Bottleneck_6_0, self).__init__()
 
-	def forward(self, featureMapToUpsample, originalFeatureMap):
-		u = self.upsample(featureMapToUpsample)
-		u = self.conv_upsample(u)
+		# using pre-normalization and pre-activation
+		# TODO: switch stride=2 between conv1 and conv2 and check results
+		# self.bn1 = nn.BatchNorm2d(out_channels)
+		self.bn1 = nn.BatchNorm2d(in_channels)
+		self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=1, bias=False)
 
-		x = self.conv1(originalFeatureMap)
-		return x + u
+		self.bn2 = nn.BatchNorm2d(out_channels)
+		self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1, bias=False)
+
+		self.bn3 = nn.BatchNorm2d(out_channels)
+		self.conv3 = nn.Conv2d(out_channels, out_channels*self.expansion, kernel_size=1, bias=False)
+
+		self.conv1_skip = nn.Conv2d(in_channels, out_channels*self.expansion, kernel_size=1, stride=2, bias=False)
+		
+		self.bn4 = nn.BatchNorm2d(out_channels*self.expansion)
+		self.conv4 = nn.Conv2d(out_channels*self.expansion, out_channels*self.expansion, kernel_size=1, bias=False)
+
+		self.bn5 = nn.BatchNorm2d(out_channels*self.expansion)
+		self.conv5 = nn.Conv2d(out_channels*self.expansion, out_channels*self.expansion, kernel_size=1, bias=False)
+
+		self.relu = nn.ReLU(inplace=True)
+
+		for m in self.modules():
+			if isinstance(m, nn.Conv2d):
+				nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+			elif isinstance(m, nn.BatchNorm2d):
+				nn.init.constant_(m.weight, 1)
+				nn.init.constant_(m.bias, 0)
+
+	def forward(self, x):
+		x = self.bn1(x)
+		x = self.relu(x)
+		# res = self.bn1_skip(res)
+		res = self.conv1_skip(x)
+
+		x = self.conv1(x)
+		x = self.bn2(x)
+		x = self.conv2(self.relu(x))
+		x = self.bn3(x)
+		x = self.conv3(self.relu(x))
+		x = x + res
+		
+		res = self.bn4(x)
+		res = self.relu(res)
+		x = self.conv4(res)
+		x = self.bn5(x)
+		x = self.conv5(self.relu(x))
+
+		out = x+res
+
+		return out
 
 
 class Upsample_2(nn.Module):
