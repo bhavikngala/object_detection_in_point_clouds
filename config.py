35e1a78ed41d19d52845762c180a1d6a45b78514
Bhavik N Gala <bhavikna@buffalo.edu>
2018-12-10 18:46:22 -0500

    new configs

--- config.py
+++ config.py
@@ -48,42 +48,11 @@ downsamplingFactor = 4
 r = int((y_max-y_min)/(gridConfig['res']*downsamplingFactor))
 c = int((x_max-x_min)/(gridConfig['res']*downsamplingFactor))
 
-Tr_velo_to_cam = np.array([
-		[7.49916597e-03, -9.99971248e-01, -8.65110297e-04, -6.71807577e-03],
-		[1.18652889e-02, 9.54520517e-04, -9.99910318e-01, -7.33152811e-02],
-		[9.99882833e-01, 7.49141178e-03, 1.18719929e-02, -2.78557062e-01],
-		[0, 0, 0, 1]
-	])
-# cal mean from train set
-R0 = np.array([
-		[0.99992475, 0.00975976, -0.00734152, 0],
-		[-0.0097913, 0.99994262, -0.00430371, 0],
-		[0.00729911, 0.0043753, 0.99996319, 0],
-		[0, 0, 0, 1]
-])
-P2 = np.array([[719.787081,         0., 608.463003,    44.9538775],
-               [        0., 719.787081, 174.545111,     0.1066855],
-               [        0.,         0.,         1., 3.0106472e-03],
-			   [0., 0., 0., 0]
-])
-R0_inv = np.linalg.inv(R0)
-Tr_velo_to_cam_inv = np.linalg.inv(Tr_velo_to_cam)
-P2_inv = np.linalg.pinv(P2)
-
 objtype = 'car'
-
-# carMeanLogWL = np.array([-0.5783, -0.0371, -0.0116, 25.1296, -2.9034, -3.8962], dtype=np.float32)
-# carSTDLogWL = np.array([0.7345, 0.3532, 0.0160, 207.0052, 0.1119, 0.0628], dtype=np.float32)
-carMeanLogWL = np.array([-0.5783, -0.0371, -0.0116, -0.0049, -2.9034, -3.8962], dtype=np.float32)
-carSTDLogWL = np.array([0.7345, 0.3532, 0.0160, 0.0059, 0.1119, 0.0628], dtype=np.float32)
-carMeanV = np.array([-0.5783, -0.0371, -0.3530, -0.3511,  2.2671,  1.4021], dtype=np.float32)
-carSTDV = np.array([0.7345, 0.3532, 0.2045, 0.2046, 0.1119, 0.0628], dtype = np.float32)
-carMean = carMeanLogWL
-carSTD = carSTDLogWL
 
 # res_block_layers = list if number of channels in the first conv layer of each res_block
 # up_sample_layers = list of tuple of number of channels input deconv and conv layers
-# deconv = tuplr of (dilation, stride, padding, output_padding) for deconvolution in upsampling
+# deconv = tuple of (dilation, stride, padding, output_padding) for deconvolution in upsampling
 
 res_block_layers = [24, 48, 64, 96]
 up_sample_layers = [(196, 256), (128, 192)]
@@ -93,6 +62,7 @@ deconv = [(1, 2, 1, 1), # upsamole block 1
 # training parameters
 lr = 1e-4   # learning rate without step
 slr = 1e-2  # step learning rate
+lrDecay = 0.1 # learning rate decay
 milestones = [150, 175] # milestone for pixor
 momentum = 0.9
 decay = 0.0001 # weight decay parameter
