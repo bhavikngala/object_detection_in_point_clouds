8c8c07b3dfe30692f55fc0e601deeee23cf2ff0a
Bhavik N Gala <bhavikna@buffalo.edu>
2018-12-10 23:52:47 -0500

    removed old functions

--- lossUtils.py
+++ lossUtils.py
@@ -73,177 +73,14 @@ def computeDistanceBetCenters(matchedBoxes, targets):
 	return d.sum().item()
 
 
-def findInOutMask_1(loc, rectangle, inside=True):
-	# rectangle is array of 4 points of 2d bounding box
-	# find vectors of 2 adjacent sides
-	# AB = (Bx-Ax, By-Ay) and so on
-	# point inside rectangle ABCD should satisfy the condition
-	# 0 <= dot(AB, AM) <= dot(AB,AB) and
-	# 0 <= dot(BC, BM) <= dot(BC,BC)
-	# AB
-	AB_x = rectangle[:, 2] - rectangle[:, 0] # Bx - Ax
-	AB_y = rectangle[:, 3] - rectangle[:, 1] # By - Ay
-
-	# BC
-	BC_x = rectangle[:, 4] - rectangle[:, 2] # Cx - Bx
-	BC_y = rectangle[:, 5] - rectangle[:, 3] # Cy - By
-
-	# AM
-	AM_x = loc[:, 2] - rectangle[:, 0] # Mx - Ax
-	AM_y = loc[:, 3] - rectangle[:, 1] # My - Ay
-
-	# BM
-	BM_x = loc[:, 2] - rectangle[:, 2] # Mx - Bx
-	BM_y = loc[:, 3] - rectangle[:, 3] # My - By
-
-	dot_AB_AM = AB_x*AM_x + AB_y*AM_y	
-	dot_AB_AB = AB_x*AB_x + AB_y*AB_y
-	dot_BC_BM = BC_x*BM_x + BC_y*BM_y
-	dot_BC_BC = BC_x*BC_x + BC_y*BC_y
-
-	if inside:
-		mask = (0<=dot_AB_AM) & (dot_AB_AM<=dot_AB_AB) & (0<=dot_BC_BM) & (dot_BC_BM<=dot_BC_BC)
-	else:
-		mask = ~((0<=dot_AB_AM) & (dot_AB_AM<=dot_AB_AB) & (0<=dot_BC_BM) & (dot_BC_BM<=dot_BC_BC))
-	
-	return mask
-
-
-def computeLoss6(cla, loc, targets, zoomed0_3, zoomed1_2, args):
-	reshape = args.standarize
-	discard = args.discard
-	only_pos = args.only_pos
-
-	posClaLoss = None
-	negClaLoss = None
-	claLoss = None
-	locLoss = None
-	md = 0
-	meanConfidence = 0
-	numPosSamples = 0
-	numNegSamples = 0
-	overallMeanConfidence = 0
-	zrr = 0
-
-	if reshape:
-		# move the channel axis to the last dimension
-		lm, lc, lh, lw = loc.size()
-		lr = lh * lw
-		loc = loc.permute(0, 2, 3, 1).contiguous().view(lm, lr, lc)
-		cla = cla.permute(0, 2, 3, 1).contiguous().view(lm, lr, 1)
-	else:
-		lm, lr, lc= loc.size()
-		cla = cla.permute(0, 2, 3, 1).contiguous().view(lm, lr, 1)
-
-	for i in range(lm):
-		zr = zoomed0_3[i].size(0)
-
-		if zr == 1 and targets[i][0,0] == -1:
-			if discard:
-				loss, oamc = focalLoss(cla[i].view(-1), 0, reduction=None, alpha=cnf.alpha)
-				loss = torch.topk(loss.view(-1), 10)[0].sum()
-				numNegSamples += 10
-			else:
-				loss, oamc = focalLoss(cla[i].view(-1), 0, reduction='sum', alpha=cnf.alpha)
-				numNegSamples += lr
-			
-			overallMeanConfidence += oamc.item()
-			if negClaLoss is not None:
-				negClaLoss += loss
-			else:
-				negClaLoss = loss
-			continue
-
-		loc1 = loc[i].repeat(1, zr).view(-1, lc)
-		cla1 = cla[i].repeat(1, zr).view(-1, 1)
-
-		zoomed0_3_1 = zoomed0_3[i].repeat(lr, 1)
-		zoomed1_2_1 = zoomed1_2[i].repeat(lr, 1)
-		targets_1 = targets[i].repeat(lr, 1)
-
-
-		#***************PS******************
-		
-		b = findInOutMask_1(loc1, zoomed0_3_1, inside=True)
-		numPosSamples1 = b.sum().item()
-		numPosSamples += numPosSamples1
-
-		if numPosSamples1>0:
-			loss, oamc = focalLoss(cla1[b], 1, reduction='sum', alpha=cnf.alpha)
-			meanConfidence += cla1[b].sum()
-			overallMeanConfidence += oamc.item()
-			if posClaLoss is not None:
-				posClaLoss += loss
-			else:
-				posClaLoss = loss
-
-			if locLoss is not None:
-				locLoss += F.smooth_l1_loss(loc1[b], targets_1[b][:,1:], reduction='sum')
-			else:
-				locLoss = F.smooth_l1_loss(loc1[b], targets_1[b][:,1:], reduction='sum')
-			md += computeDistanceBetCenters(loc1[b], targets_1[b])
-				
-		#***************PS******************
-
-		#***************NS******************
-		
-		b1 = findInOutMask_1(loc1, zoomed1_2_1, inside=False)
-		b1 = b1.view(lr, zr).sum(dim=-1)==zr
-		numNegSamples1 = b1.sum().item()
-
-		if numNegSamples1>0:
-			cla1 = cla1.view(lr, 1*zr)
-
-			if discard:
-				loss, oamc = focalLoss(cla1[b1][:,0], 0, reduction=None, alpha=cnf.alpha)
-				loss = torch.topk(loss.view(-1), 10)[0].sum()
-				numNegSamples += 10
-			else:
-				loss, oamc = focalLoss(cla1[b1][:,0], 0, reduction='sum', alpha=cnf.alpha)
-				numNegSamples += numNegSamples1
-			
-			overallMeanConfidence += oamc.item()
-			if negClaLoss is not None:
-				negClaLoss += loss
-			else:
-				negClaLoss = loss
-
-		#***************NS******************
-		zrr += zr
-	
-	if numPosSamples>0:
-		meanConfidence /= numPosSamples
-		md /= numPosSamples
-	if numPosSamples!=0 or numNegSamples!=0:
-		overallMeanConfidence /=(numPosSamples+numNegSamples)
-
-	if numPosSamples > 0 and numNegSamples > 0:
-		posClaLoss /= numPosSamples
-		negClaLoss /= numNegSamples
-		claLoss = posClaLoss + negClaLoss
-		locLoss /= numPosSamples
-		claLoss /= cnf.accumulationSteps
-		locLoss /= cnf.accumulationSteps
-	elif numNegSamples > 0:
-		negClaLoss /= numNegSamples
-		claLoss = negClaLoss
-		claLoss /= cnf.accumulationSteps
-
-	# discard loss if there are no positive samples
-	if only_pos and numPosSamples==0:
-		claLoss = None
-
-	return claLoss, locLoss, posClaLoss, negClaLoss, md, meanConfidence, overallMeanConfidence, numPosSamples, numNegSamples, zr
-
-
 def computeLoss7(cla, loc, targetClas, targetLocs, zoomed0_3, zoomed1_2, args):
-	only_pos = args.only_pos
+	only_pos = False
 
 	posClaLoss = None
 	negClaLoss = None
 	claLoss = None
 	locLoss = None
-	md = 0
+	trainLoss = None
 	meanConfidence = 0
 	numPosSamples = 0
 	numNegSamples = 0
@@ -294,8 +131,7 @@ def computeLoss7(cla, loc, targetClas, targetLocs, zoomed0_3, zoomed1_2, args):
 			locLoss += F.smooth_l1_loss(predL, targetL, reduction='sum')
 		else:
 			locLoss = F.smooth_l1_loss(predL, targetL, reduction='sum')
-		# md += computeDistanceBetCenters(loc1[b], targets_1[b])
-				
+						
 		#***************PS******************
 
 		#***************NS******************
@@ -316,7 +152,6 @@ def computeLoss7(cla, loc, targetClas, targetLocs, zoomed0_3, zoomed1_2, args):
 	numNegSamples = lm*lr - numPosSamples
 	if numPosSamples>0:
 		meanConfidence /= numPosSamples
-		# md /= numPosSamples
 	if numPosSamples!=0 or numNegSamples!=0:
 		overallMeanConfidence /=(numPosSamples+numNegSamples)
 
@@ -325,15 +160,17 @@ def computeLoss7(cla, loc, targetClas, targetLocs, zoomed0_3, zoomed1_2, args):
 		negClaLoss /= (numNegSamples*cnf.accumulationSteps)
 		claLoss = posClaLoss + negClaLoss
 		locLoss /= (numPosSamples*cnf.accumulationSteps)
+		trainLoss = claLoss + locLoss
 	elif numNegSamples > 0:
 		negClaLoss /= (numNegSamples*cnf.accumulationSteps)
 		claLoss = negClaLoss
+		trainLoss = claLosss
 
 	# discard loss if there are no positive samples
 	if only_pos and numPosSamples==0:
 		claLoss = None
 
-	return claLoss, locLoss, posClaLoss, negClaLoss, md, meanConfidence, overallMeanConfidence, numPosSamples, numNegSamples
+	return claLoss, locLoss, trainLoss, posClaLoss, negClaLoss, meanConfidence, overallMeanConfidence, numPosSamples, numNegSamples
 
 
 def focalLoss(p, t, reduction=None, alpha = None):
