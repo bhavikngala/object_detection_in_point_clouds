7de3d862f16e9b5f4c036e0553b73201efa0cf7f
Bhavik N Gala <bhavikna@buffalo.edu>
2018-12-21 20:54:22 -0500

    plot boxes on img

--- datautils/kitti_utils.py
+++ datautils/kitti_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+import cv2
 
 
 class KittiReader():
@@ -163,3 +164,29 @@ def cart2hom(pts_3d):
 	n = pts_3d.shape[0]
 	pts_3d_hom = np.hstack((pts_3d, np.ones((n,1))))
 	return pts_3d_hom
+
+
+def draw_projected_box3d(image, qs, color=(255,255,255), thickness=2):
+	''' Draw 3d bounding box in image
+		qs: (8,3) array of vertices for the 3d box in following order:
+			1 -------- 0
+		   /|		 /|
+		  2 -------- 3 .
+		  | |		| |
+		  . 5 -------- 4
+		  |/		 |/
+		  6 -------- 7
+	'''
+	qs = qs.astype(np.int32)
+	for k in range(0,4):
+	   # Ref: http://docs.enthought.com/mayavi/mayavi/auto/mlab_helper_functions.html
+	   i,j=k,(k+1)%4
+	   # use LINE_AA for opencv3
+	   cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+
+	   i,j=k+4,(k+1)%4 + 4
+	   cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+
+	   i,j=k,k+4
+	   cv2.line(image, (qs[i,0],qs[i,1]), (qs[j,0],qs[j,1]), color, thickness, cv2.LINE_AA)
+	return image
